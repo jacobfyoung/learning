868a198b8227626f3a63f303b8f80c81938d91a7
jacobfyoung <jacobfyoung@gmail.com>
2026-02-03 11:16:01 +0000

    Add maps page component

---
+++ src/routes/maps/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  let { data } = $props();
+</script>
+
+<h1>My Maps</h1>
+
+<ul>
+  {#each data.maps as map}
+    <li>{map.name}</li>
+  {:else}
+    <p>No maps found. Try adding one in the Supabase dashboard!</p>
+  {/each}
+</ul>
+
+
